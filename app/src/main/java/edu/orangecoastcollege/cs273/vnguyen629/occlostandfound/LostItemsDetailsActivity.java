b5a89839954024023f6c18da24b16e1b725939e7
vnguyen629 <brotherflab@gmail.com>
2016-11-10 15:31:05 -0800

    New Activites

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/vnguyen629/occlostandfound/LostItemsDetailsActivity.java
@@ -0,0 +1,13 @@
+package edu.orangecoastcollege.cs273.vnguyen629.occlostandfound;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LostItemsDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lost_items_details);
+    }
+}
