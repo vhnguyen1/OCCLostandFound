4e6a5e00dc873a4a13da3181c4dd96df755f543a
vnguyen678 <unreal136@gmail.com>
2016-11-17 15:59:59 -0800

    testing

--- app/src/main/java/edu/orangecoastcollege/cs273/vnguyen629/occlostandfound/UserAccount.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/vnguyen629/occlostandfound/UserAccount.java
@@ -4,4 +4,8 @@ package edu.orangecoastcollege.cs273.vnguyen629.occlostandfound;
  * Created by vhngu on 11/10/2016.
  */
 public class UserAccount {
+    String mStudentUserName;
+    String mStudentPassword;
+    int    mStudentID;
+
 }
