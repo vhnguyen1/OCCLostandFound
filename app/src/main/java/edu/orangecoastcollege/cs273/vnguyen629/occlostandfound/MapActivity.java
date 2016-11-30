8b2f0b5ff646106b8849b2b6aa83a1578e812f6e
bnguyen336 <binhnguyen1092@gmail.com>
2016-11-29 16:39:11 -0800

    Adding MapActivity.java and activity_map.xml for Google Map API

--- app/src/main/java/edu/orangecoastcollege/cs273/vnguyen629/occlostandfound/MapActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/vnguyen629/occlostandfound/MapActivity.java
@@ -1,38 +1,47 @@
 package edu.orangecoastcollege.cs273.vnguyen629.occlostandfound;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.ImageView;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  *
  */
-public class MapActivity extends AppCompatActivity {
-    private ImageView mapImageView;
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    /**
-     *
-     * @param savedInstanceState
-     */
+    private GoogleMap mMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        mapImageView = (ImageView) findViewById(R.id.mapImageView);
+        SupportMapFragment googleMapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.googleMapFragment);
+
+        googleMapFragment.getMapAsync(this);
     }
 
-    /**
-     *
-     * @param view
-     */
-    public void mapChange(View view) {
-        if (mapImageView.getDrawable() == null)
-            mapImageView.setImageDrawable(getDrawable(R.drawable.occ_map_full));
-         else
-            mapImageView.setImageDrawable(null);
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        String officeTitle = getString(R.string.office_title);
 
+        LatLng coordinate = new LatLng(33.669694, -117.909295);
+        mMap.addMarker(new MarkerOptions().position(coordinate).title(officeTitle));
+
+        //Change camera view to the marker
+        CameraPosition cameraPosition =
+                new CameraPosition.Builder().target(coordinate).zoom(14.0f).build();
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+        mMap.moveCamera(cameraUpdate);
     }
 }
