1e6b388f3b9e3d114a53962f74f2d3157c402326
vnguyen629 <brotherflab@gmail.com>
2016-12-04 18:31:11 -0800

    Major changes to ui, new icons and images, new activities, the second sensor/accelerometer, and also and new layouts.

--- app/src/main/java/edu/orangecoastcollege/cs273/vnguyen629/occlostandfound/MainMenuActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/vnguyen629/occlostandfound/MainMenuActivity.java
@@ -1,6 +1,9 @@
 package edu.orangecoastcollege.cs273.vnguyen629.occlostandfound;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,6 +12,10 @@ import android.view.View;
  *
  */
 public class MainMenuActivity extends AppCompatActivity {
+    private Sensor accelerometer;
+    private SensorManager sensorManager;
+    private ShakeDetector shakeDetector;
+
     /**
      *
      * @param savedInstanceState
@@ -17,6 +24,41 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        shakeDetector = new ShakeDetector(new ShakeDetector.OnShakeListener() {
+            /**
+             * When a 3D motion that the sensors constitute as a shake has been detected,
+             * the ItemListActivity is loaded
+             */
+            @Override
+            public void onShake() {
+                startActivity(new Intent(MainMenuActivity.this, ItemsListActivity.class));
+            }
+        });
+    }
+
+    /**
+     * When the user re-enters the app, the sensors start back up and begin
+     * monitoring device movements/g-forces in a 3D (x-y-z) span
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(shakeDetector, accelerometer,
+                SensorManager.SENSOR_DELAY_UI);
+    }
+
+    /**
+     * When the user switches apps or clicks on the home button without closing the app,
+     * all the sensors that monitor device movements and g-forces are then paused
+     * to preserve battery life
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(shakeDetector);
     }
 
     /**
