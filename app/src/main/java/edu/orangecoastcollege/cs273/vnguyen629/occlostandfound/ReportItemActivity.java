042c8f1b5e146bdb158fc90d17312f066bca2371
vnguyen629 <brotherflab@gmail.com>
2016-12-05 17:05:09 -0800

    Layout and model changes

--- app/src/main/java/edu/orangecoastcollege/cs273/vnguyen629/occlostandfound/ReportItemActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/vnguyen629/occlostandfound/ReportItemActivity.java
@@ -1,20 +1,104 @@
 package edu.orangecoastcollege.cs273.vnguyen629.occlostandfound;
 
+import android.Manifest;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.AnyRes;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+
+import static android.R.attr.data;
+
+/**
+ *
+ */
 public class ReportItemActivity extends AppCompatActivity {
+    private Uri imageUri;
+    private ImageView reportItemImageView;
 
+    private static final int REQUEST_CODE = 13;
+
+    /**
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_item);
+
+        imageUri = getUriToResource(this, R.drawable.defaultImage);
+        reportItemImageView.setImageURI(imageUri);
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data != null && requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            imageUri = data.getData();
+            reportItemImageView.setImageURI(imageUri);
+        }
+    }
+
+    /**
+     *
+     * @param view
+     */
+    public void selectItemImage(View view) {
+        ArrayList<String> permList =  new ArrayList<>();
+
+        int cameraPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (cameraPermission != PackageManager.PERMISSION_GRANTED)
+            permList.add(Manifest.permission.CAMERA);
+
+        int writePermission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (writePermission != PackageManager.PERMISSION_GRANTED)
+            permList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        int readPermission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (readPermission != PackageManager.PERMISSION_GRANTED)
+            permList.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if (permList.size() > 0) {
+            String[] perms = new String[permList.size()];
+            ActivityCompat.requestPermissions(this, permList.toArray(perms), REQUEST_CODE);
+        }
+
+        if (cameraPermission == PackageManager.PERMISSION_GRANTED
+                && writePermission == PackageManager.PERMISSION_GRANTED
+                && readPermission == PackageManager.PERMISSION_GRANTED) {
+            Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(galleryIntent, REQUEST_CODE);
+        }
+        else {
+            if (cameraPermission != PackageManager.PERMISSION_GRANTED
+                    && writePermission != PackageManager.PERMISSION_GRANTED
+                    && readPermission != PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this, "OCC Lost and Found requires camera and " +
+                        "external storage permissions.", Toast.LENGTH_SHORT).show();
+            else if (cameraPermission != PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this, "OCC Lost and Found requires camera permissions.",
+                        Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(this, "OCC Lost and Found requires external storage permissions.",
+                        Toast.LENGTH_SHORT).show();
+        }
     }
 
     /**
