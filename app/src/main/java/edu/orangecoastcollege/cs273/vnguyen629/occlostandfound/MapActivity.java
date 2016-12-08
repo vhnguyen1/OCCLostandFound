258e9cc8cc80b94336b4ad0e90635c8228530160
bnguyen336 <binhnguyen1092@gmail.com>
2016-12-08 13:56:42 -0800

    Fixing GoogleMap

--- app/src/main/java/edu/orangecoastcollege/cs273/vnguyen629/occlostandfound/MapActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/vnguyen629/occlostandfound/MapActivity.java
@@ -12,17 +12,13 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-/**
- *
- */
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    public static final double latitude = 33.669658;
+    public static final double longtitude = -117.909386;
 
     private GoogleMap mMap;
 
-    /**
-     *
-     * @param savedInstanceState The state of the application saved into a bundle
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,21 +30,16 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         googleMapFragment.getMapAsync(this);
     }
 
-    /**
-     *
-     * @param googleMap
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
+        //Use Latitude and Longtitude to create marker on map
         mMap = googleMap;
-        String officeTitle = getString(R.string.office_title);
+        LatLng coordinate = new LatLng(latitude,longtitude);
+        mMap.addMarker(new MarkerOptions().position(coordinate).title(getString(R.string.office_title)));
 
-        LatLng coordinate = new LatLng(33.669694, -117.909295);
-        mMap.addMarker(new MarkerOptions().position(coordinate).title(officeTitle));
-
-        //Change camera view to the marker
-        CameraPosition cameraPosition =
-                new CameraPosition.Builder().target(coordinate).zoom(14.0f).build();
+        //Change the camera view to the marker
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(coordinate).zoom(14.0f).build();
         CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
         mMap.moveCamera(cameraUpdate);
     }
