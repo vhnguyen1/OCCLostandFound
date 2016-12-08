78ec89b7b86aec1d49ab2376ed4112177270695c
bnguyen336 <binhnguyen1092@gmail.com>
2016-12-08 15:19:28 -0800

    Fix GoogleMap

--- app/src/main/java/edu/orangecoastcollege/cs273/vnguyen629/occlostandfound/GoogleMapsActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/vnguyen629/occlostandfound/GoogleMapsActivity.java
@@ -10,14 +10,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class GoogleMapActivity extends FragmentActivity implements OnMapReadyCallback {
+public class GoogleMapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_google_map);
+        setContentView(R.layout.activity_google_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
